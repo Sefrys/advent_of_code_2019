b47b9446b76925f0e05c5f96423676b55a857060
Ireneusz Zagan <sefrys@gmail.com>
2019-12-16 15:13:10 +0100

    add missing files

---
+++ src/day_1.rs
@@ -0,0 +1,28 @@
+use crate::read_file;
+
+fn calculate_fuel_req(x: u32) -> u32 {
+    (x / 3) as u32 - 2
+}
+
+pub fn calculate_sum_of_fuel_req(filename: &str) -> u32 {
+   read_file(filename).lines().map(|x| calculate_fuel_req(x.parse::<u32>().unwrap())).sum()
+}
+
+
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fuel_req() {
+        assert_eq!(654, calculate_fuel_req(1969));
+        assert_eq!(33583, calculate_fuel_req(100756))
+    }
+
+    #[test]
+    fn test_sum_fuel_req() {
+        assert_eq!(34237, calculate_sum_of_fuel_req("inputs/day_1_test.txt"));
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+mod day_1;
+
+use std::fs;
+
+pub fn read_file(filename: &str) -> String {
+    fs::read_to_string(filename).unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_read_file() {
+        let str = reader::read_file("test.txt");
+        assert_eq!("test_1", str.lines().nth(0).unwrap());
+    }
+}
+
